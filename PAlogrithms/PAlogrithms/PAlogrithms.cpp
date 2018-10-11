efe97a2755010ff6c825f91c729b95ab4045d1b5
R Vijaya Bhaskar <vijay.smartph@gmail.com>
2018-10-11 16:43:30 +0530

    check for references
    check for references

--- PAlogrithms/PAlogrithms/PAlogrithms.cpp
+++ PAlogrithms/PAlogrithms/PAlogrithms.cpp
@@ -1,9 +1,34 @@
 #include <stdio.h>
-
+#include<iostream>
 using namespace std;
+
+struct CS {
+	int x;
+
+	CS() {
+		x = 100;
+		cout << "==== CS constr" << endl;
+	}
+};
+
+CS& fun(CS c) {
+	
+	c.x = 1;
+
+	return c;
+}
+
+void fun2(CS& x) {
+
+	cout << x.x << endl;
+
+}
 
 int main()
 {
   cout << "hi";
+  CS c;
+  fun2(fun(c));
   return 0;
 }
+
